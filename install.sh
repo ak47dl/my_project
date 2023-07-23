1334bf4f0d6a069f563a34f7a0dd20ba187f9b2e
DUY <thanhduy.cs@gmail.com>
2023-07-24 00:45:06 +0700

    changed

--- install.sh
+++ install.sh
@@ -93,8 +93,10 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
+# echo "How many proxy do you want to create? Example 500"
+# read COUNT
+COUNT=100
+echo "Creating $COUNT proxies ipv6"
 
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
